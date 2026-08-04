78a54a396a3aa06970219cb77bd74e642ae77a94
Chase Vallery <Chase1527@gmail.com>
2026-08-04 13:07:45 -0600

    testing

---
+++ testing.py
@@ -0,0 +1,10 @@
+def show_greeting(name):
+    print("Hello, " + name)
+
+def create_greeting(name):
+    return "Welcome, " + name
+
+message1 = show_greeting("Alice")
+message2 = create_greeting("Bob")
+
+print(message1)
